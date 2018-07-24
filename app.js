82cad866b551ecf220c0e5152573e3889f6f1060
ben <behnam.pournasr@farmersinsurance.com>
2018-07-24 14:30:47 -0700

    add mongo

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const db = mongoose.connect('mongodb://localhost/bookAPI');
+
+const Book = require('./models/bookModel');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const bookRouter = express.Router();
+
+bookRouter.route('/Books')
+  .get((req, res) => {
+      Book.find((err, books) => {
+        if(err) {
+          res.status(500).send(err);
+        } else {
+          res.json(resJson)
+        }
+      })
+  });
+
+app.use('/api', bookRouter);
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+app.listen(port, () => {
+  console.log(port);
+});
